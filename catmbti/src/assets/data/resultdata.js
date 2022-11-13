636421b1aa75ad74d2a59fd94d4fa6c75710be37
givesilverstick <silverstick21@sookmyung.ac.kr>
2022-11-14 02:09:29 +0900

    add result page

---
+++ catmbti/src/assets/data/resultdata.js
@@ -0,0 +1,132 @@
+import Abyssinian from '../cat/abyssinian.jpg';
+import Ameshort from '../cat/ameshort.jpg';
+import Bengal from '../cat/bengal.jpg';
+import British from '../cat/british.jpg';
+import Exotic from '../cat/exotic.jpg';
+import Korshort from '../cat/korshort.jpg';
+import Mainecoon from '../cat/mainecoon.jpg';
+import Munchkin from '../cat/munchkin.jpg';
+import Norwegianforest from '../cat/norwegianforest.jpg';
+import Persian from '../cat/persian.png';
+import Ragdoll from '../cat/ragdoll.jpg';
+import RussianBlue from '../cat/russianblue.png';
+import Scottishfold from '../cat/scottishfold.jpg';
+import Siamese from '../cat/siamese.jpg';
+import Sphinx from '../cat/sphinx.jpg';
+import Turkishangora from '../cat/turkishangora.jpg';
+
+export const ResultData = [
+    {
+        id: 1,
+        name: '러시안 블루',
+        best: 'ESTJ', 
+        desc: '겁이 많아요. 워낙 온수난 성격에 조용한 고양이라 소심...?',
+        image: `${RussianBlue}`,
+    },
+    {
+        id: 2,
+        name: '샴',
+        best: 'ESFP', 
+        desc: '대체적으로 온순하고 느긋한 성격이에요. 외로움을 잘타며 ...?',
+        image: `${Siamese}`,
+    },
+    {
+        id: 3,
+        name: '페르시안',
+        best: 'ENTJ', 
+        desc: '조용하고 성격도 순한편이어서 개냥이가 되는 경우가 많습니다..?',
+        image: `${Persian}`,
+    },
+    {
+        id: 4,
+        name: '벵갈',
+        best: 'INTP', 
+        desc: '활동력이 어마어마한 고양이에요! 그렇기 때문에 살이 ..?',
+        image: `${Bengal}`,
+    },
+    {
+        id: 5,
+        name: '터키쉬 앙고라',
+        best: 'INTJ', 
+        desc: '우아한 장모종 고양이! 그러다보니 까칠하고 성질이 조금..?',
+        image: `${Turkishangora}`,
+    },
+    {
+        id: 6,
+        name: '아비시니안',
+        best: 'ISTJ', 
+        desc: '민첩하고 호기심 많고 똑똑한 고양이입니다! 굉장히 수다..?',
+        image: `${Abyssinian}`,
+    },
+    {
+        id: 7,
+        name: '노르웨이 숲',
+        best: 'ESTP', 
+        desc: '조용하고 사람을 매우 좋아하는 고양이 중 하나! 숲에서 ...?',
+        image: `${Norwegianforest}`,
+    },
+    {
+        id: 8,
+        name: '아메리칸 숏헤어',
+        best: 'INFP', 
+        desc: '주인에 대한 호기심이 높고 애교가 굉장히 많아요! 다만!...?',
+        image: `${Ameshort}`,
+    },
+    {
+        id: 9,
+        name: '코리안 숏헤어',
+        best: 'ISFP', 
+        desc: '많은 고양이의 혈통이 섞여있는 고양이에요! 대부분 활발..?',
+        image: `${Korshort}`,
+    },
+    {
+        id: 10,
+        name: '엑죠틱',
+        best: 'ISTJ', 
+        desc: '낯을 가리지 않고, 주인 옆에서 느긋하게 일상을 즐기는 ..?',
+        image: `${Exotic}`,
+    },
+    {
+        id: 11,
+        name: '스코티쉬 폴드',
+        best: 'ENFP', 
+        desc: '접힌 귀가 굉장히 귀여운 고양이! 착하고 느긋하며 울음..?',
+        image: `${Scottishfold}`,
+    },
+    {
+        id: 12,
+        name: '',
+        best: '', 
+        desc: '',
+        image: `${Munchkin}`,
+    },
+    {
+        id: 13,
+        name: '렉돌',
+        best: 'ESFJ', 
+        desc: '긴 털과 아주 예쁜 외모로 많이 사랑받는 고양이! 봉제인형..?',
+        image: `${Ragdoll}`,
+    },
+    {
+        id: 14,
+        name: '브리티쉬 숏헤어',
+        best: 'ENTP', 
+        desc: '이름과 같이 영국 신사와 같은 고양이! 배려심이 깊고 ..?',
+        image: `${British}`,
+    },
+    {
+        id: 15,
+        name: '스핑크스',
+        best: 'ISTP', 
+        desc: '유일하게 털이 없는 고양이입니다! 외모와는 다르게 고양..?',
+        image: `${Sphinx}`,
+    },
+    {
+        id: 16,
+        name: '메인쿤',
+        best: 'ENFJ', 
+        desc: '거인 신사라는 별명을 가진 고양이에요! 조용하고 온화한...?',
+        image: `${Mainecoon}`,
+    },
+
+]
--- catmbti/src/pages/Result.js
+++ catmbti/src/pages/Result.js
@@ -1,8 +1,63 @@
 import React from 'react';
+// css-in-js
+import styled from 'styled-components';
+import PangImage from '../assets/haz.jpg';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import { ResultData } from '../assets/data/resultdata';
 
 const Result = () => {
+    const navigate = useNavigate();
 
-    return(<div>결과화면입니다.</div>)
+    return(
+        <Wrapper>
+            <Header>예비집사 판별기</Header>
+            <Contents>
+                <Title>결과 보기</Title>
+                <LogoImage>
+                    <img src={ResultData[0].image} className="rounded-circle" width={350} height={350}></img>
+                </LogoImage>
+                <Desc>예비 집사님과 찰떡궁합인 고양이는 {ResultData[0].name}입니다.</Desc>
+                <Button style={{ fontFamily: "EF_jejudoldam" }} onClick={() => navigate("/")}>테스트 다시하기</Button>
+            </Contents>
+        </Wrapper>
+    )
 }
 
 export default Result;
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+`
+
+const Header = styled.div`
+    font-size: 40pt;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "EF_jejudoldam";
+`
+
+const Contents = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+const Title = styled.div`
+    font-size: 30pt;
+    margin-top: 40px;
+    font-family: "EF_jejudoldam";
+`
+
+const LogoImage = styled.div`
+    margin-top: 10px;
+`
+
+const Desc = styled.div`
+    font-size: 20pt;
+    margin-top: 30px;
+    font-family: "EF_jejudoldam";
+`

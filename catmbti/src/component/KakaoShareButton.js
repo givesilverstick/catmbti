75136fd96b11646b0c786e5d16b47a5a6f191f3a
givesilverstick <silverstick21@sookmyung.ac.kr>
2022-11-22 02:10:38 +0900

    edit kakao message

--- catmbti/src/component/KakaoShareButton.js
+++ catmbti/src/component/KakaoShareButton.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-const {Kakao} = window;
+const { Kakao } = window;
 
 const KakaoShareButton = () => {
     const url = "https://catmbti21.netlify.app/"
     const resultUrl = window.location.href;
-
-    console.log('ddd', resultUrl, url)
 
     React.useEffect(()=>{
       Kakao.cleanup();
       Kakao.init("8152154ad517e9a78e8847e701180057");
       console.log(Kakao.isInitialized());
     }, []);
-    Kakao.Share.sendDefault({
+
+    const shareKakao = () => {
+      Kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
           title: '예비집사 판별기 결과',
-          description: '예비 집사님이 고양이를 키운다면 가장 잘맞는 고양이는 아비시니안입니다.',
-          imageUrl:
-            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+          description: '예비 집사님이 고양이를 키운다면 가장 잘맞는 고양이는 ${data.name}입니다.',
+          imageUrl: url + data.image,
           link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            webUrl: 'https://developers.kakao.com',
+            mobileWebUrl: resultUrl,
+            webUrl: resultUrl,
           },
         },
         buttons: [
@@ -35,6 +34,8 @@ const KakaoShareButton = () => {
           },
         ],
       });
+    }
+    
     return(
         <Button style={{ fontFamily: "EF_jejudoldam", width: 170, marginLeft: '20px' }} >카카오톡 공유하기</Button>
     )
